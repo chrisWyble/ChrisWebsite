cbce7c229cb28c816161d033d7e5749de5161721
chrisWyble <44722856+chrisWyble@users.noreply.github.com>
2018-11-03 04:52:08 -0500

    1

---
+++ scripts/console.js
@@ -0,0 +1,34 @@
+console.log(`%c
+. . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . 
+.                                                            .
+.   ooooooooooooooooooooooooooooooooooooooooooooooooooooo    .
+.   ooooooooooooooooooooooooooooooooooooooooooooooooooooo    .
+.   ooooooooooooooooooooooooooooooooooooooooooooooooooooo    .
+.   oooooooooooooooooooooo:,...';looooooooooooooooooooooo    .
+.   oooooooooooooooooool.          .;oooooooooooooooooooo    .
+.   oooooooooooooooooo.       .';;;'  ,oooooooooooooooooo    .
+.   ooooooooooooooool     ,dKMMMMMMMNc .ooooooooooooooooo    .
+.   oooooooooooooooo. ;klXMMMMMMMMMMMMo .oooooooooooooooo    .
+.   oooooooooooooooo 'MMMMMMMMMMMMMMMMW. oooooooooooooooo    .
+.   oooooooooooooooo.:MMMMMMMMMMMMMMMMM0 oooooooooooooooo    .
+.   oooooooooooooooo:.MNK0OO0MMMOl:,'',o.0ooooooooooooooo    .
+.   ooooooooooooooook..      ,K0        WXooooooooooooooo    .
+.   oooooooooooooooooK       lMMo     ,OMkooooooooooooooo    .
+.   ooooooooooooooooodKo:::o0MMMMMNXWMMXOoooooooooooooooo    .
+.   oooooooooooooooooodKMMMMMMMMMMMMMMMdooooooooooooooooo    .
+.   ooooooooooooooooooodWMMMMMMMMMMMMMXoooooooooooooooooo    .
+.   oooooooooooooooooooocXMMMMMMMMMMMN. loooooooooooooooo    .
+.   ooooooooooooooooooo.  ;XMMMMMMMM0.  'oooooooooooooooo    .
+.   ooooooooooooooooooo   xoxXWMMMX;      .',:loooooooooo    .
+.   ooooooooooooooo:'.    .WMMMWO;              ..';coooo    .
+.   ooooooooooc,.          .NK,                        .'    .
+.   ooooool;.               .l.                              .
+.   oool'                    .,                              .
+.   oo,                      cd                              .
+.   o;                                                       .
+.   c                                                        .
+.                                                            .
+.                        Andrew Ma                           .
+.                   andrew-ma.github.io                      .
+.                                                            .
+. . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .`,"color:#FF4246; background:black; font-family: monospace");
